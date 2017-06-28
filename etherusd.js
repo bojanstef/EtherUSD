b43ac9484f66880b2087442c267e74a0a87ca8c6
Bojan Stefanovic <bojanstef4@gmail.com>
2017-06-28 14:58:35 -0700

    Added express server and changed to tweet every minute

--- etherusd.js
+++ etherusd.js
@@ -1,29 +1,36 @@
-var port = process.env.PORT || 8080;
+const express = require('express');
+const app = express();
 
-var schedule = require('node-schedule');
+const schedule = require('node-schedule');
 
-var Coinbase = require('coinbase').Client;
-var coinbase_client = new Coinbase({
+const Coinbase = require('coinbase').Client;
+const coinbase_client = new Coinbase({
 	'apiKey': 'YPa0WtPxisDx6ScT',
 	'apiSecret': 'hlpnSYpvlrOMyNoZkNe6ryG9kNk4WdsA'
 });
 
-var Twitter = require('twitter');
-var twitter_client = new Twitter({
-  consumer_key: 'vdmrnHiiq4GI1DIwauSJHdQkj',
-  consumer_secret: 'gFF3C6yWpkcCI5kovk74fFBXa6V2naIYdTdMHZwkQL8bUdtKnb',
-  access_token_key: '879561476121411584-2s7EE8JkTngX4K8kzFo0EKiC06fE4Ky',
-  access_token_secret: 'YeJRKN9FPJDjbBJM5j5QZSxw73ivCLETJzlqsiwle7wlc'
-})
+const Twitter = require('twitter');
+const twitter_client = new Twitter({
+	consumer_key: 'vdmrnHiiq4GI1DIwauSJHdQkj',
+	consumer_secret: 'gFF3C6yWpkcCI5kovk74fFBXa6V2naIYdTdMHZwkQL8bUdtKnb',
+	access_token_key: '879561476121411584-2s7EE8JkTngX4K8kzFo0EKiC06fE4Ky',
+	access_token_secret: 'YeJRKN9FPJDjbBJM5j5QZSxw73ivCLETJzlqsiwle7wlc'
+});
 
-var j = schedule.scheduleJob('*/1 * * * *', function() {
-	coinbase_client.getExchangeRates({'currency': 'ETH'}, function(err, rates) {
-		var exchange = rates['data']['rates']['USD'];
-		var tweet_text = "ETH: $" + exchange;
+app.get('/', function (req, res) {
+	const job = schedule.scheduleJob('*/1 * * * *', function() {
+		coinbase_client.getExchangeRates({'currency': 'ETH'}, function(err, rates) {
+			const exchange = rates['data']['rates']['USD'];
+			const tweet_text = "ETH: $" + exchange;
 
-		twitter_client.post('statuses/update', {status: tweet_text}, function(error, tweet, response) {
-	  		if (!error) console.log();
+			twitter_client.post('statuses/update', {status: tweet_text}, function(error, tweet, response) {
+		  		if (!error) console.log();
+			});
 		});
 	});
+});
+
+app.listen(3000, function () {
+  console.log('EtherUSD listening on port 3000!');
 });
 
