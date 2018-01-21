1a3d7a8540ba180c3fff2294d6755bc2333bfc9d
Bojan Stefanovic <bojanstef4@gmail.com>
2018-01-21 15:01:32 -0800

    moved to index

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+const schedule = require('node-schedule');
+const request = require('request');
+const Twitter = require('twitter');
+const twitter_client = new Twitter({
+	consumer_key: 'vdmrnHiiq4GI1DIwauSJHdQkj',
+	consumer_secret: 'gFF3C6yWpkcCI5kovk74fFBXa6V2naIYdTdMHZwkQL8bUdtKnb',
+	access_token_key: '879561476121411584-2s7EE8JkTngX4K8kzFo0EKiC06fE4Ky',
+	access_token_secret: 'YeJRKN9FPJDjbBJM5j5QZSxw73ivCLETJzlqsiwle7wlc'
+});
+
+app.listen(port, function () {
+    const job = schedule.scheduleJob('*/20 * * * *', function() {
+        request.get('https://api.coinmarketcap.com/v1/ticker/ethereum/', function(err, res, body) {
+            const json = JSON.parse(body);
+            const price = json[0].price_usd;
+            const tweet_text = '$ETH :' + price + 'USD';
+            twitter_client.post('statuses/update', {status: tweet_text}, function(error, tweet, response) {
+            	if (!error) console.log();
+            })
+        })
+	});
+});
